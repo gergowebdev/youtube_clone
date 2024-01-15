f01567c158ae4b4de6b89b40c7f56e4dc463e7f0
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-01-15 21:25:05 +0100

    Creating the SearchBar and the Feed components

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
-import { SearchIcon } from "@mui/icons-material";
+import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
     return (
@@ -22,7 +22,9 @@ const SearchBar = () => {
                 value=""
                 onChange={() => {}}
             />
-            <IconButton></IconButton>
+            <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+                <Search />
+            </IconButton>
         </Paper>
     );
 };
