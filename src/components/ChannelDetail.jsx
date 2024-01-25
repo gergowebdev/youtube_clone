a49a97493d3b1b561a15c4be662b51eff08d4a3a
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-01-25 20:46:27 +0100

    Creating the ChannelDetail component

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Videos, ChannelCard } from "./";
+import { fetchFromApi } from "../utils/fetchFromApi";
 
 const ChannelDetail = () => {
-    return <div>ChannelDetail</div>;
+    const [channelDetail, setChannelDetail] = useState(null);
+    const [videos, setVideos] = useState([]);
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetchFromApi(`channels?part=snippet&id=${id}`).then((data) =>
+            setChannelDetail(data?.items[0])
+        );
+        fetchFromApi(`search?channelId=${id}&part=snippet&order=date`).then(
+            (data) => setVideos(data?.items)
+        );
+    }, [id]);
+
+    return <Box minHeight="95vh"></Box>;
 };
 
 export default ChannelDetail;
